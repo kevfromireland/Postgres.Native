ddebcd155c061f4f824c184eebf6ba284c1a5dfe
kevfromireland <kevin.boyle@outlook.com>
2016-09-03 12:11:56 +0100

    Initial commit of Postgres binary provider

---
+++ Postgres.Native/Postgres.Native.Tests/PostgresNativeTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Postgres.Native.Tests
+{
+    [TestFixture]
+    public class PostgresBinariesExtractorTests
+    {
+        [Test]
+        public async Task CanExtractPostgres()
+        {
+            var pgLocation = await new PostgresBinariesExtractor().ExtractToTemporaryPath();
+
+            Assert.That(File.Exists(Path.Combine(pgLocation.FullName, "pgsql", "bin", "postgres.exe")),
+                "Unable to find postgres executable after extracting");
+        }
+    }
+}
---
+++ Postgres.Native/Postgres.Native/PostgresBinariesExtractor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Postgres.Native
+{
+    public class PostgresBinariesExtractor
+    {
+        public Task<DirectoryInfo> ExtractToTemporaryPath()
+        {
+            var pgAssembly = Assembly.GetExecutingAssembly();
+
+            var pgStream = pgAssembly.GetManifestResourceStream("Postgres.Native.postgresql-9.5.4-1-windows-x64-binaries.zip");
+
+            if (pgStream == null)
+            {
+                throw new InvalidOperationException("Unable to find expected binary embedded resource");
+            }
+
+            var temp = Path.GetTempPath();
+            var pgDestination = Path.Combine(temp, Path.GetRandomFileName());
+
+            var zipArchive = new ZipArchive(pgStream);
+            zipArchive.ExtractToDirectory(pgDestination);
+
+            return Task.FromResult(new DirectoryInfo(pgDestination));
+        }
+    }
+}
